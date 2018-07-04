ff8db4647df9dd80c17f6fd92af6f0582ca17830
Volha Valasevich <volha_valasevich@epam.com>
2018-07-04 10:30:34 +0300

    move character check to a separate function, add check for id + small fixes

--- getchar/util.js
+++ getchar/util.js
@@ -6,44 +6,54 @@ class Util {
     }
 
     getData(url) {
-        let result = [];            
+        let result = [];
         const getchars = (url) => request(url, { json: true })  //recursive function for getting characters from all pages
             .then((body) => {
                 result = result.concat(body.results);           //saving results from current page
                 if (body.info.next !== "") {                    //checking if it is the last page
                     return getchars(body.info.next);            //if not, get characters from the next page
-                } else return result;                       
+                } else return result;
             })
 
-        return getchars(url);                                   
+        return getchars(url);
     }
 
     search(args) {
         return new Promise((resolve, reject) => {
-            const keys = this.getKeys(args);                //get all the parameters for search
             try {
                 this.getData(this.url).then((data) => {        //get all data
                     const chars = data.map((el) => {           //creating a new array of characters
-                        let flag = true;                       //flag to indicate that current character fits the criteria
-                        for (let i = 0; i < keys.length; i++) {                         //checking multiple parameters
-                            if (keys[i] === 'origin' || keys[i] === 'location') {       //"origin" and "location" parameters are objects
-                                if (el[keys[i]].name.indexOf(args[keys[i]]) === -1) {   //and have to be checked by their .name property
-                                    flag = false;              //if one parameter does not fit the criteria, character is marked as not right                                      
-                                    break;                     //and cycle breaks
-                                }
-                            } else {
-                                if (el[keys[i]].indexOf(args[keys[i]]) === -1) {
-                                    flag = false;
-                                    break;
-                                }
-                            }
-                        }
-                        if (flag === true) { return el; }       //if the character is marked as right, return it to the result array
+                        return this.compare(el, args);         //check current element by all the parameters
                     })
                     resolve(chars);
                 })
             } catch (err) { reject(err); }
         })
+    }
+
+    compare(el, args) {
+        const keys = this.getKeys(args);                                //get all the parameters for search
+        let flag = true;                                                //flag to indicate that current character fits the criteria
+        for (let i = 0; i < keys.length; i++) {                         //checking multiple parameters
+            if (keys[i] == "id") {
+                if (el[keys[i]].toString() !== args[keys[i]].toString()) {       //id is a number and has to be an exact match
+                    flag = false;                   //if one parameter does not fit the criteria, character is marked as not right
+                    break;                          //and cycle breaks
+                }
+            }
+            else if (keys[i] === 'origin' || keys[i] === 'location') {       //"origin" and "location" parameters are objects
+                if (el[keys[i]].name.indexOf(args[keys[i]]) === -1) {   //and have to be checked by their .name property
+                    flag = false;
+                    break;
+                }
+            } else {
+                if (el[keys[i]].indexOf(args[keys[i]]) === -1) {
+                    flag = false;
+                    break;
+                }
+            }
+        }
+        if (flag === true) { return el; }       //if the character is marked as right, return it to the result array
     }
 
     getKeys(args) {
@@ -65,7 +75,7 @@ class Util {
         data.forEach((el) => {
             result += `Id: ${el.id};\nName: ${el.name};\nStatus: ${el.status};\nSpecies: ${el.species};\nType: ${el.type};\nGender: ${el.gender};\nOrigin: ${el.origin.name};\nLocation: ${el.location.name}\n\n`
         })
-        return result.slice(0, -1);
+        return result.slice(0, -1);        //removing the last \n to make the text look better in the terminal
     }
 
 }
